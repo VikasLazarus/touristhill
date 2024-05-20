2a6d50fcdab8964262ac806b2b5069db327bae91
VikasLazarus <148435329+VikasLazarus@users.noreply.github.com>
2024-05-20 16:40:13 +0530

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,31 @@
+var myObject= {"customers":[
+    {"Name" : "Single Family Suite","price" : "6,000.00","link" : "packages/pa1.html","d1" : "De Mount Castle","d2" : "With Balcony","img" : "https://img.freepik.com/free-photo/winding-road-cuts-path-through-lush-green-mountains-hazy-sky_91128-4483.jpg?t=st=1716192137~exp=1716195737~hmac=d8528207ff19ac52b64a1a44f250f5c346c3eca41cfccf8396f30ac97601eac4&w=900"},
+    {"Name" : "Double Family Suite","price" : "6,000.00","link" : "packages/pa1.html","d1" : "De Mount Castle","d2" : "With Balcony","img" : "https://img.freepik.com/free-photo/winding-road-cuts-path-through-lush-green-mountains-hazy-sky_91128-4483.jpg?t=st=1716192137~exp=1716195737~hmac=d8528207ff19ac52b64a1a44f250f5c346c3eca41cfccf8396f30ac97601eac4&w=900"},
+    {"Name" : "Best Family Suite","price" : "6,000.00","link" : "packages/pa1.html","d1" : "De Mount Castle","d2" : "With Balcony","img" : "https://img.freepik.com/free-photo/winding-road-cuts-path-through-lush-green-mountains-hazy-sky_91128-4483.jpg?t=st=1716192137~exp=1716195737~hmac=d8528207ff19ac52b64a1a44f250f5c346c3eca41cfccf8396f30ac97601eac4&w=900"},
+    {"Name" : "New Family Suite","price" : "6,000.00","link" : "packages/pa1.html","d1" : "De Mount Castle","d2" : "With Balcony","img" : "https://img.freepik.com/free-photo/winding-road-cuts-path-through-lush-green-mountains-hazy-sky_91128-4483.jpg?t=st=1716192137~exp=1716195737~hmac=d8528207ff19ac52b64a1a44f250f5c346c3eca41cfccf8396f30ac97601eac4&w=900"},
+    
+    ]};
+
+    var myObject2= {"customers2":[
+        {"Name" : "Single Family Suite","price" : "6,000.00","link" : "packages/pa1.html","d1" : "De Mount Castle","d2" : "With Balcony","img" : "https://img.freepik.com/free-photo/winding-road-cuts-path-through-lush-green-mountains-hazy-sky_91128-4483.jpg?t=st=1716192137~exp=1716195737~hmac=d8528207ff19ac52b64a1a44f250f5c346c3eca41cfccf8396f30ac97601eac4&w=900"},
+        {"Name" : "Double Family Suite","price" : "6,000.00","link" : "packages/pa1.html","d1" : "De Mount Castle","d2" : "With Balcony","img" : "https://img.freepik.com/free-photo/winding-road-cuts-path-through-lush-green-mountains-hazy-sky_91128-4483.jpg?t=st=1716192137~exp=1716195737~hmac=d8528207ff19ac52b64a1a44f250f5c346c3eca41cfccf8396f30ac97601eac4&w=900"},
+        {"Name" : "Best Family Suite","price" : "6,000.00","link" : "packages/pa1.html","d1" : "De Mount Castle","d2" : "With Balcony","img" : "https://img.freepik.com/free-photo/winding-road-cuts-path-through-lush-green-mountains-hazy-sky_91128-4483.jpg?t=st=1716192137~exp=1716195737~hmac=d8528207ff19ac52b64a1a44f250f5c346c3eca41cfccf8396f30ac97601eac4&w=900"},
+        {"Name" : "New Family Suite","price" : "6,000.00","link" : "packages/pa1.html","d1" : "De Mount Castle","d2" : "With Balcony","img" : "https://img.freepik.com/free-photo/winding-road-cuts-path-through-lush-green-mountains-hazy-sky_91128-4483.jpg?t=st=1716192137~exp=1716195737~hmac=d8528207ff19ac52b64a1a44f250f5c346c3eca41cfccf8396f30ac97601eac4&w=900"},
+        
+        ]};
+
+        var myObject3= {"customers3":[
+            {"Name" : "Single Family Suite","price" : "6,000.00","link" : "packages/pa1.html","d1" : "De Mount Castle","d2" : "With Balcony","img" : "https://img.freepik.com/free-photo/winding-road-cuts-path-through-lush-green-mountains-hazy-sky_91128-4483.jpg?t=st=1716192137~exp=1716195737~hmac=d8528207ff19ac52b64a1a44f250f5c346c3eca41cfccf8396f30ac97601eac4&w=900"},
+            {"Name" : "Double Family Suite","price" : "6,000.00","link" : "packages/pa1.html","d1" : "De Mount Castle","d2" : "With Balcony","img" : "https://img.freepik.com/free-photo/winding-road-cuts-path-through-lush-green-mountains-hazy-sky_91128-4483.jpg?t=st=1716192137~exp=1716195737~hmac=d8528207ff19ac52b64a1a44f250f5c346c3eca41cfccf8396f30ac97601eac4&w=900"},
+            {"Name" : "Best Family Suite","price" : "6,000.00","link" : "packages/pa1.html","d1" : "De Mount Castle","d2" : "With Balcony","img" : "https://img.freepik.com/free-photo/winding-road-cuts-path-through-lush-green-mountains-hazy-sky_91128-4483.jpg?t=st=1716192137~exp=1716195737~hmac=d8528207ff19ac52b64a1a44f250f5c346c3eca41cfccf8396f30ac97601eac4&w=900"},
+            {"Name" : "New Family Suite","price" : "6,000.00","link" : "packages/pa1.html","d1" : "De Mount Castle","d2" : "With Balcony","img" : "https://img.freepik.com/free-photo/winding-road-cuts-path-through-lush-green-mountains-hazy-sky_91128-4483.jpg?t=st=1716192137~exp=1716195737~hmac=d8528207ff19ac52b64a1a44f250f5c346c3eca41cfccf8396f30ac97601eac4&w=900"},
+            
+            ]};
+
+            var Popular= {"packages":[
+                {"Name" : "Single Family Suite","price" : "6,000.00","link" : "packages/pa1.html","d1" : "De Mount Castle","d2" : "With Balcony","img" : "https://img.freepik.com/free-photo/winding-road-cuts-path-through-lush-green-mountains-hazy-sky_91128-4483.jpg?t=st=1716192137~exp=1716195737~hmac=d8528207ff19ac52b64a1a44f250f5c346c3eca41cfccf8396f30ac97601eac4&w=900"},
+                {"Name" : "Double Family Suite","price" : "6,000.00","link" : "packages/pa1.html","d1" : "De Mount Castle","d2" : "With Balcony","img" : "https://img.freepik.com/free-photo/winding-road-cuts-path-through-lush-green-mountains-hazy-sky_91128-4483.jpg?t=st=1716192137~exp=1716195737~hmac=d8528207ff19ac52b64a1a44f250f5c346c3eca41cfccf8396f30ac97601eac4&w=900"},
+                {"Name" : "Best Family Suite","price" : "6,000.00","link" : "packages/pa1.html","d1" : "De Mount Castle","d2" : "With Balcony","img" : "https://img.freepik.com/free-photo/winding-road-cuts-path-through-lush-green-mountains-hazy-sky_91128-4483.jpg?t=st=1716192137~exp=1716195737~hmac=d8528207ff19ac52b64a1a44f250f5c346c3eca41cfccf8396f30ac97601eac4&w=900"},
+                {"Name" : "New Family Suite","price" : "6,000.00","link" : "packages/pa1.html","d1" : "De Mount Castle","d2" : "With Balcony","img" : "https://img.freepik.com/free-photo/winding-road-cuts-path-through-lush-green-mountains-hazy-sky_91128-4483.jpg?t=st=1716192137~exp=1716195737~hmac=d8528207ff19ac52b64a1a44f250f5c346c3eca41cfccf8396f30ac97601eac4&w=900"},
+                
+                ]};
